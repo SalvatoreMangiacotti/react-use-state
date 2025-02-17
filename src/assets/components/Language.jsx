2e5be1a8ad330c2272e21350bf0fcd877a816402
Salvatore <workstudioms@gmail.com>
2025-02-17 14:50:39 +0100

    languages buttons, done

---
+++ src/assets/components/Language.jsx
@@ -0,0 +1,13 @@
+export default function LanguageAccordion(props) {
+
+    return (
+
+        <>
+            <span className="accordion-btn">
+                {props.title}
+            </span>
+        </>
+
+    )
+
+}
